54e758084cf606867c82cd0bccc9b7ffcc9fb5d3
Ensar Emir EROL <ensaremir.erol99@gmail.com>
2021-06-25 19:59:56 +0300

    Adde MainDecoder

--- ARM.srcs/sources_1/imports/FSM.srcs/sources_1/new/register.sv
+++ ARM.srcs/sources_1/imports/FSM.srcs/sources_1/new/register.sv
@@ -24,20 +24,13 @@ module register(input clk, input [2:0] A1, A2, A3, input reg [7:0] WD3, input lo
     output reg [7:0] RD1, RD2
     );  
     reg [7:0] register [7:0];
-    reg [2:0] a1, a2, a3;
-    flop4bits flopa1(clk, A1, a1);
-    flop4bits flopa2(clk, A2, a2);
-    flop4bits flopa3(clk, A3, a3);
     always @ (posedge clk)
-        if(WE3 && clk) register[a3]<=WD3;
-    assign RD1 = register[a1];
-    assign RD2 = register[a2];
+    begin
+        if(WE3) register[A3]<=WD3;
+        RD1 <= register[A1];
+        RD2 <= register[A2];
+    end
     initial begin
-        for(int i = 0; i < $size(register); i++)begin
-            register[i] = 0;
-        end
-        a1 = A1;
-        a2 = A2;
-        a3 = A3;
+        for(int i = 0; i < $size(register); i++) register[i] = 0;
     end
 endmodule
---
+++ ARM.srcs/sources_1/new/MainDecoder.sv
@@ -0,0 +1,177 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06/25/2021 07:31:57 PM
+// Design Name: 
+// Module Name: MainDecoder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module MainDecoder(
+    input reg [3:0] op,
+    output reg PCS, MEMW, REGW, dNOW, dPOP, dPUSH, ALUOP,
+    output reg ImmSrc, ZeroSrc, AluSrc,
+    output reg [2:0] RegSrc,
+    output reg [1:0] ResultSel, MemWDSel
+    );
+    parameter ALU = 4'b0000;
+    parameter ALU_IMM = 4'b1000;
+    parameter SHH = 4'b0001;
+    parameter SHH_IMM = 4'b1001;
+    parameter LDR = 4'b0010;
+    parameter LDR_IMM = 4'b1010;
+    parameter STR = 4'b0011;
+    parameter STR_IMM = 4'b1011;
+    parameter CMP = 4'b0100;
+    parameter CMP_IMM = 4'b1100;
+    parameter PUSH = 4'b0101;
+    parameter PUSH_PC = 4'b1101;
+    parameter POP = 4'b0110;
+    parameter RET = 4'b1110;
+    parameter JMP = 4'b0111;
+    parameter SKP = 4'b1111;
+    
+    always @ (*)
+        case(op)
+            ALU: begin
+                PCS = 0;
+                ResultSel = 0;
+                MEMW = 0;
+                ImmSrc = 0;
+                ZeroSrc = 0;
+                REGW = 1;
+                RegSrc = 0;
+                dNOW = 0;
+                dPOP = 0;
+                dPUSH = 0;
+                MemWDSel = 0;
+                ALUOP = 1;
+                AluSrc = 0;
+                end
+                
+            ALU_IMM: begin
+                PCS = 0;
+                ResultSel = 0;
+                MEMW = 0;
+                ImmSrc = 1;
+                ZeroSrc = 0;
+                REGW = 1;
+                RegSrc = 0;
+                dNOW = 0;
+                dPOP = 0;
+                dPUSH = 0;
+                MemWDSel = 0;
+                ALUOP = 1;
+                AluSrc = 0;
+                end
+                
+            SHH: begin
+                PCS = 0;
+                ResultSel = 0;
+                MEMW = 0;
+                ImmSrc = 0;
+                ZeroSrc = 0;
+                REGW = 1;
+                RegSrc = 0;
+                dNOW = 0;
+                dPOP = 0;
+                dPUSH = 0;
+                MemWDSel = 0;
+                ALUOP = 1;
+                AluSrc = 1;
+                end
+                
+            SHH_IMM: begin
+                PCS = 0;
+                ResultSel = 0;
+                MEMW = 0;
+                ImmSrc = 1;
+                ZeroSrc = 0;
+                REGW = 1;
+                RegSrc = 0;
+                dNOW = 0;
+                dPOP = 0;
+                dPUSH = 0;
+                MemWDSel = 0;
+                ALUOP = 1;
+                AluSrc = 1;
+                end
+                
+            LDR: begin
+                PCS = 0;
+                ResultSel = 1;
+                MEMW = 0;
+                ImmSrc = 1;
+                ZeroSrc = 0;
+                REGW = 1;
+                RegSrc = 0;
+                dNOW = 0;
+                dPOP = 0;
+                dPUSH = 0;
+                MemWDSel = 0;
+                ALUOP = 0;
+                AluSrc = 0;
+                end
+                
+            LDR_IMM: begin
+                PCS = 0;
+                ResultSel = 2;
+                MEMW = 0;
+                ImmSrc = 0;
+                ZeroSrc = 0;
+                REGW = 1;
+                RegSrc = 0;
+                dNOW = 0;
+                dPOP = 0;
+                dPUSH = 0;
+                MemWDSel = 0;
+                ALUOP = 0;
+                AluSrc = 0;
+                end
+                
+            STR: begin
+                PCS = 0;
+                ResultSel = 0;
+                MEMW = 1;
+                ImmSrc = 1;
+                ZeroSrc = 0;
+                REGW = 0;
+                RegSrc = 1;
+                dNOW = 0;
+                dPOP = 0;
+                dPUSH = 0;
+                MemWDSel = 0;
+                ALUOP = 0;
+                AluSrc = 0;
+                end
+                
+            STR_IMM: begin
+                PCS = 0;
+                ResultSel = 0;
+                MEMW = 1;
+                ImmSrc = 0;
+                ZeroSrc = 1;
+                REGW = 0;
+                RegSrc = 1;
+                dNOW = 0;
+                dPOP = 0;
+                dPUSH = 0;
+                MemWDSel = 1;
+                ALUOP = 0;
+                AluSrc = 0;
+                end
+        endcase
+endmodule
